c49e49286ef6cee89aa677b476e7a148a94b1b5c
MrDeef <aleksanrovich211@mail.ru>
2024-05-08 16:02:56 +0700

    g

--- BlazorAppTest/BlazorAppTest/obj/Debug/net8.0/BlazorAppTest.AssemblyInfo.cs
+++ BlazorAppTest/BlazorAppTest/obj/Debug/net8.0/BlazorAppTest.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorAppTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2d2d983695cabc115da19ce89d82ec307580b54")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8283fc3beea41d5e276e0b7d6605705c7e9659a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorAppTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorAppTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
